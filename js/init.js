2e6c6a2dc8e83e0508628df02818c7193fa8f083
Sofia sanchez <sofi1494@outlook.es>
2021-09-05 18:15:48 -0300

    realice filtados y agregue nombre de usuario

--- js/init.js
+++ js/init.js
@@ -6,6 +6,7 @@ const PRODUCT_INFO_URL = "https://japdevdep.github.io/ecommerce-api/product/5678
 const PRODUCT_INFO_COMMENTS_URL = "https://japdevdep.github.io/ecommerce-api/product/5678-comments.json";
 const CART_INFO_URL = "https://japdevdep.github.io/ecommerce-api/cart/987.json";
 const CART_BUY_URL = "https://japdevdep.github.io/ecommerce-api/cart/buy.json";
+var miStorage = window.localStorage;
 
 var showSpinner = function(){
   document.getElementById("spinner-wrapper").style.display = "block";
@@ -39,9 +40,13 @@ var getJSONData = function(url){
         return result;
     });
 }
-
+function muestraEmail(){
+  let login = localStorage.getItem('userName');
+    document.getElementById("muestroUsuario").innerHTML += login
+}
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+  muestraEmail()
 });
--- js/products.js
+++ js/products.js
@@ -1,29 +1,142 @@
+const ORDER_ASC_BY_COST = "ASC";
+const ORDER_DESC_BY_COST = "DESC";
+const ORDER_BY_SOLD_COUNT = "Cant.";
+var currentProductsArray = [];
+var currentSortCriteria = undefined;
+var minCount = undefined;
+var maxCount = undefined;
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+
 document.addEventListener("DOMContentLoaded", function (e) {
+
+    document.getElementById("sortAsc").addEventListener("click", function () {
+        sortAndShowProducts(ORDER_ASC_BY_COST);
+    });
+
+    document.getElementById("sortDesc").addEventListener("click", function () {
+        sortAndShowProducts(ORDER_DESC_BY_COST);
+    });
+
+    document.getElementById("sortByCount").addEventListener("click", function () {
+        sortAndShowProducts(ORDER_BY_SOLD_COUNT);
+    });
+
+    document.getElementById("clearRangeFilter").addEventListener("click", function(){
+        document.getElementById("rangeFilterCountMin").value = "";
+        document.getElementById("rangeFilterCountMax").value = "";
+
+        minCount = undefined;
+        maxCount = undefined;
+
+        generarTabla();
+    });
+
+    document.getElementById("rangeFilterCount").addEventListener("click", function(){
+        //Obtengo el mínimo y máximo de los intervalos para filtrar por precio
+        minCount = document.getElementById("rangeFilterCountMin").value;
+        maxCount = document.getElementById("rangeFilterCountMax").value;
+
+        if ((minCount != undefined) && (minCount != "") && (parseInt(minCount)) >= 0){
+            minCount = parseInt(minCount);
+        }
+        else{
+            minCount = undefined;
+        }
+
+        if ((maxCount != undefined) && (maxCount != "") && (parseInt(maxCount)) >= 0){
+            maxCount = parseInt(maxCount);
+        }
+        else{
+            maxCount = undefined;
+        }
+
+        generarTabla();
+    });
 
     //llamada a la funcion asincrona
     getProducts()
 
 });
+
+function sortProducts(criteria, array) {
+    let result = [];
+    if (criteria === ORDER_ASC_BY_COST) {
+        result = array.sort(function (a, b) {
+            if (a.cost < b.cost) {
+                return -1;
+            }
+            if (a.cost > b.cost) {
+                return 1;
+            }
+            return 0;
+        });
+    } else if (criteria === ORDER_DESC_BY_COST) {
+        result = array.sort(function (a, b) {
+            if (a.cost > b.cost) {
+                return -1;
+            }
+            if (a.cost < b.cost) {
+                return 1;
+            }
+            return 0;
+        });
+    } else if (criteria === ORDER_BY_SOLD_COUNT) {
+        result = array.sort(function (a, b) {
+            let aCount = parseInt(a.soldCount);
+            let bCount = parseInt(b.soldCount);
+
+            if (aCount > bCount) {
+                return -1;
+            }
+            if (aCount < bCount) {
+                return 1;
+            }
+            return 0;
+        });
+    }
+
+    return result;
+}
+
+function sortAndShowProducts(sortCriteria, productsArray) {
+    currentSortCriteria = sortCriteria;
+
+    if (productsArray != undefined) {
+        currentProductsArray = productsArray;
+    }
+
+    currentProductsArray = sortProducts(currentSortCriteria, currentProductsArray);
+
+    //Muestro los productos ordenados
+    generarTabla();
+}
+
 //funcion asincronica que espera la respuesta de la api de productos 
 let getProducts = async () => {
-    //invoca la funcion getJSONData y espera la respuesta de la URL
+    //invoca la funcion getJSONData, espera la respuesta de la URL y la guarda en la variable
+    // currentProductsArray
     let response = await getJSONData(PRODUCTS_URL)
-    generar_tabla(response.data)
-
+    currentProductsArray = response.data
+    generarTabla()
 };
 
 
-let generar_tabla = (data) => {
+let generarTabla = () => {
     // Obtener la referencia del elemento container
-    var container = document.getElementsByClassName("container p-5")[0]
+     var container = document.getElementsByClassName("container p-5")[0]
+    // Borro el contenido del contenedor para reemplazarlo con la nueva informacion
+    container.innerHTML= " "
     // Crea un elemento <table> y un elemento <tbody>
     var tabla = document.createElement("table");
     var tblBody = document.createElement("tbody");
     // Crea las celdas
-    data.forEach(element => {
+    currentProductsArray.forEach(element => {
+        if (((minCount == undefined) || (minCount != undefined && parseInt(element.cost) >= minCount)) &&
+            ((maxCount == undefined) || (maxCount != undefined && parseInt(element.cost) <= maxCount))){
         var hilera = document.createElement("tr");
         var celda = document.createElement("td");
         const img = new Image(200, 100); // width, height
@@ -36,12 +149,12 @@ let generar_tabla = (data) => {
         createTd(element.name, hilera, tblBody)
         createTd(element.description, hilera, tblBody)
         createTd(`${element.currency} ${element.cost}`, hilera, tblBody)
-
+            }
     })
     tabla.appendChild(tblBody);
 
     container.appendChild(tabla);
-    // modifica el atributo "border" de la tabla y lo fija a "2";
+    // modifica el atributo "border" de la tabla y lo fija a "1";
     tabla.setAttribute("border", "1");
 }
 
@@ -54,4 +167,3 @@ let createTd = (text, hilera, tblBody) => {
     tblBody.appendChild(hilera);
 
 }
-
